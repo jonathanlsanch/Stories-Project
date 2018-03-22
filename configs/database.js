84cf76dbf33a429c0cadda86ff460323127cb7df
Jonathan Sanchez <jonathansanchez@Jonathans-MacBook-Pro.local>
2018-03-22 15:24:17 -0400

    deploy test 3

--- configs/database.js
+++ configs/database.js
@@ -6,7 +6,8 @@ const mongoose = require('mongoose');
 const dbName = 'stories-database';
 
 // connect to the database
-mongoose.connect(`mongodb://localhost/${dbName}`);
+// mongoose.connect(`mongodb://localhost/${dbName}`);
+mongoose.connect(process.env.MONGODB_URI);
 
 const db = mongoose.connection;
 
